247aa87ce327643395c928145fa72a03b98fdc14
mike <mike@developer.com>
2024-10-16 01:49:07 -0300

    + add artifact build

--- src/Main.kt
+++ src/Main.kt
@@ -2,20 +2,22 @@ import migration.SyntheticMigration
 import java.io.File
 
 
+/**
+ * based on file path received as parameter, read file content, extract layout file name and ids
+ * then replace it in the content
+ */
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
         println("Please provide the file path as a parameter.")
         return
     }
-    //based on file path received as parameter, read file content, extract layout file name and ids
-    //then replace it in the content
-    val filePath = args[0]
-    val layoutfilePath = args[1]
+    val layoutFilePath = args[0]
+    val filePath = args[1]
     val syntheticMigration = SyntheticMigration()
     val content = syntheticMigration.readFile(filePath)
     val layoutFileName = syntheticMigration.extractLayoutFileName(content)
 //    println("layoutFileName:$layoutFileName")
-    val layoutFileNamePath = "$layoutfilePath\\$layoutFileName.xml"
+    val layoutFileNamePath = "$layoutFilePath\\$layoutFileName.xml"
 //    println("layoutFileNamePath:$layoutFileNamePath")
     val layout = syntheticMigration.readFile(layoutFileNamePath)
     val ids = syntheticMigration.extractIdsFromLayout(layout)
